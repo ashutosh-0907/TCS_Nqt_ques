529581f80252d68ca70d5377c43d6be7b7f2cfa6
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2025-02-22 16:16:12 +0000

    string to int and float

--- 1_str_to_int.cpp
+++ 1_str_to_int.cpp
@@ -1,6 +1,7 @@
 //1. ++++++ using for loop  +++++++++++
 
 /*#include <iostream>
+#include<string>
 using namespace std;
 
 int main() {
@@ -20,7 +21,7 @@ int main() {
 // 2. using streamstring class
 
 
-#include <iostream>
+/*#include <iostream>
 #include <sstream>
 using namespace std;
 
@@ -35,4 +36,51 @@ int main() {
     cout<<res;
 
     return 0;
+}*/
+
+// 3. Using stoi string to int and atoi argument to int only after c++11
+/*#include<stdlib.h>
+int main(){
+    char* str  = "1246";
+    int res;
+    res = atoi(str);
+    cout<<res;
+    return 0;
+}*/
+
+// string to decimal
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+    string s = "546.09";
+    float res = 0.0;
+    float decimal = 0.1;
+    bool isdecimal = false;
+    for(int i = 0; i < s.length(); i++){
+
+        if(s[i] == '.'){
+
+            isdecimal = true;
+        }
+        else if(s[i] >= '0' && s[i] <= '9'){
+
+            if(!isdecimal){
+
+               res = res * 10 + (s[i] - '0');
+
+            }
+            else{
+                res = res  + (s[i] - '0') * decimal;
+                decimal /= 10;
+            }
+        }
+    }
+    cout << res;
+    return 0;
+
 }
